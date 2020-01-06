3d71d37b211c7c4e0b274178872b5c3c8285f608
Hoang Tung Son <hoangtungson164@gmail.com>
2020-01-06 18:08:15 +0700

    change data to mysql

--- routes/items.js
+++ routes/items.js
@@ -2,18 +2,35 @@ const express = require('express');
 var cors = require('cors');
 const router = express.Router();
 
-const data = [
-    {id: 1, title: 'Finalize project', order: 1, completed: false},
-    {id: 2, title: 'Book ticket to London', order: 2, completed: false},
-    {id: 3, title: 'Finish last article', order: 3, completed: false},
-    {id: 4, title: 'Get a new t-shirt', order: 4, completed: false},
-    {id: 5, title: 'Create dinner reservation', order: 5, completed: false},
-];
+// const data = [
+//     {id: 1, title: 'Finalize project', order: 1, completed: false},
+//     {id: 2, title: 'Book ticket to London', order: 2, completed: false},
+//     {id: 3, title: 'Finish last article', order: 3, completed: false},
+//     {id: 4, title: 'Get a new t-shirt', order: 4, completed: false},
+//     {id: 5, title: 'Create dinner reservation', order: 5, completed: false},
+// ];
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "mydb",
+    port: 3307,
+});
 
 router.use(cors());
 
 router.get('/', function (req, res) {
-    res.status(200).json(data);
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM customers", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+          res.status(200).json({ result });
+        });
+      });
 });
 
 router.get('/:id', function (req, res) {
